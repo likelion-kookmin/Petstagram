446e80dfca4880e06c35a685229e7a04aa4ab6fb
Gtavexir <gtavexir@kookmin.ac.kr>
2021-06-30 01:48:15 +0900

    feat. Add Feed Models

--- Petstagram/petstagram/models.py
+++ Petstagram/petstagram/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Feed(models.Model):
+    title = models.CharField(max_length=50)
+    create_date = models.DateTimeField(null=True)
+    writer = models.CharField(max_length=20, null=True)
+    context = models.TextField()
+    media = models.ImageField(upload_to="feed/", blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.text[:100]
