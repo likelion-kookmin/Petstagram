5992bd4ba1d304df05d6ad9c87217df8cc18204d
Gtavexir <gtavexir@kookmin.ac.kr>
2021-06-29 20:15:45 +0900

    .

--- Petstagram/petstagram/models.py
+++ Petstagram/petstagram/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Feed(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.text[:100]
--- Petstagram/petstagram/views.py
+++ Petstagram/petstagram/views.py
@@ -1,4 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Feed
 
 def index(request):
     return render(request, 'petstagram/index.html')
+
+def home(request):
+    feeds = Feed.objects.all()
+    return render(request, 'petstagram/index.html', {'feeds':feeds})
+
+def detail(request, feed_id):
+    feed = get_object_or_404(Feed, pk=feed_id)
+    return render(request, 'petstagram/detail.html', {'feed':feed})
+
+def new(request):
+    return render(request, 'petstagram/new.html')
+
+def create(request):
+    new_feed = Feed()
+    new_feed.title = request.POST['title']
+    new_feed.text = request.POST['text']
+    new_feed.save()
+    return redirect('/')
+
+def delete(request, feed_id):
+    delete_feed = get_object_or_404(Feed, pk=feed_id)
+    delete_feed.delete()
+    return redirect('/')
