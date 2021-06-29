ee6aee65ecff5dd8879bcc790de4f504ca066070
Gtavexir <gtavexir@kookmin.ac.kr>
2021-06-30 02:39:18 +0900

    feat. 새 글 작성 버튼 연결

--- Petstagram/petstagram/urls.py
+++ Petstagram/petstagram/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    # path('', views.index, name='index'),
+    path('', views.home, name="home"),
+    path('feed/new', views.new, name="new"),
+    path('feed/create', views.create, name="create"),
 ] 
--- Petstagram/petstagram/views.py
+++ Petstagram/petstagram/views.py
@@ -1,4 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Feed
 
 def index(request):
     return render(request, 'petstagram/index.html')
+
+def home(request):
+    feeds = Feed.objects.all()
+    return render(request, 'petstagram/index.html', {'feeds':feeds})
+
+def detail(request, feed_id):
+    feed = get_object_or_404(Feed, pk=feed_id)
+    return render(request, 'petstagram/detail.html', {'feed':feed})
+
+def new(request):
+    return render(request, 'petstagram/new.html')
+
+def create(request):
+    new_feed = Feed()
+    new_feed.title = request.POST['title']
+    new_feed.context = request.POST['context']
+    new_feed.create_date = timezone.datetime.now()
+    try:
+        new_feed.media = request.FILES['media']
+    except:
+        pass
+    new_feed.save()
+    return redirect('/')
+
+def delete(request, feed_id):
+    delete_feed = get_object_or_404(Feed, pk=feed_id)
+    delete_feed.delete()
+    return redirect('/')
