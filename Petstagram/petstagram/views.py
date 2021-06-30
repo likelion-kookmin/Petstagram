bb1375753be816fbdbd3f3c8258def0c52e17673
minjj0905 <minjj0905@kookmin.ac.kr>
2021-06-30 14:13:02 +0900

    feat: feed model에 username, userid 추가

--- Petstagram/petstagram/views.py
+++ Petstagram/petstagram/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.apps import apps
 from .models import Feed
 
 def index(request):
@@ -17,8 +18,13 @@ def new(request):
     return render(request, 'petstagram/new.html')
 
 def create(request):
+    CustomUser = apps.get_model('account', 'CustomUser')
+    user = CustomUser.objects.get(username=request.user) #로그인한 유저 정보 가져오기
+
     new_feed = Feed()
     new_feed.title = request.POST['title']
+    new_feed.author_id = user
+    new_feed.author_name = user.nickname     #유저 닉네임으로 작성자 저장
     new_feed.context = request.POST['context']
     new_feed.create_date = timezone.datetime.now()
     try:
